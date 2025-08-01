8112b24afab4743310ba28c24673ff7893884fa8
xthan <ethanpeacock@aol.com>
2025-08-01 12:54:35 -0500

    start writing a cuda distributor for python systems, so we can use subprocess for password and sending cmds

---
+++ computationalDistribution/go.py
@@ -0,0 +1,16 @@
+import os
+
+comps = ["Blue","A9+"]
+numbers = ["192.168.137.66", "192.168.137.67"]
+
+
+os.system("ssh u0_a288@192.168.137.67 -p 8022")
+
+import time
+time.sleep(3)
+os.system("5597")
+time.sleep(5)
+os.system("cd x && touch hi")
+os.system("exit")
+print(9)
+
